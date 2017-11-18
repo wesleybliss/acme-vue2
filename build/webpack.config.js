59c544562a17febb0275b7f4d7dc449488f06d94
Wesley Bliss <wesley.bliss@gmail.com>
2017-11-18 17:37:34 -0500

    Fixed a missing import

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const config = require('./build.config')
-const basicAuth = require('../../server/src/utils/basic-auth')
+// const basicAuth = require('../../server/src/utils/basic-auth')
 
 /**
  * @todo
